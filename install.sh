8f9d3d059bdaf33741cf88057211c9cbb4ada321
Adrian Kosmaczewski <adrian@akosma.com>
2012-01-10 15:59:16 +0100

    Better installation, by removing the old symlinks first

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+rm ~/.bash_profile
+rm ~/.gvimrc.after
+rm ~/.tcshrc
+rm ~/.vimrc.after
+
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.gvimrc.after ~/.gvimrc.after
 ln -s ~/.dotfiles/.tcshrc ~/.tcshrc
