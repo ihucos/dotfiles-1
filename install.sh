edab2754bad4994ee4fdedd433747df3b868a0bd
Adrian Kosmaczewski <adrian@akosma.com>
2012-01-24 11:29:34 +0100

    Added the installation of external Vim plugins

--- install.sh
+++ install.sh
@@ -10,3 +10,21 @@ ln -s ~/.dotfiles/.gvimrc.after ~/.gvimrc.after
 ln -s ~/.dotfiles/.tcshrc ~/.tcshrc
 ln -s ~/.dotfiles/.vimrc.after ~/.vimrc.after
 
+# Install Vim .janus extensions
+DIR=~/.janus
+
+# If the build directory exists, delete it
+if [ -d "$DIR" ]; then
+  rm -rf $DIR
+fi
+
+mkdir $DIR
+cd $DIR
+
+git clone git://github.com/joestelmach/javaScriptLint.vim.git
+git clone git://github.com/tpope/vim-ragtag.git
+git clone git://github.com/msanders/cocoa.vim.git
+git clone git://github.com/Rip-Rip/clang_complete.git
+git clone git://github.com/vim-scripts/loremipsum.git
+git clone git://github.com/godlygeek/csapprox.git
+
