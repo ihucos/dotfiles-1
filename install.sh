c5369ec5b73509fe89d4c09260bcac29e3f407e9
Adrian Kosmaczewski <adrian@akosma.com>
2012-03-31 23:20:46 +0200

    Disabled a plugin for the moment

--- install.sh
+++ install.sh
@@ -34,5 +34,7 @@ git clone git://github.com/Rip-Rip/clang_complete.git
 git clone git://github.com/vim-scripts/loremipsum.git
 git clone git://github.com/vim-scripts/YankRing.vim.git
 git clone git://github.com/Lokaltog/vim-powerline.git
-git clone git://github.com/kana/vim-smartinput.git
+
+# Disabled for the moment, as it makes command line vim sluggish (?)
+# git clone git://github.com/kana/vim-smartinput.git
 
